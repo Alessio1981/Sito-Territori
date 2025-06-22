d4846cd789082e15f21c42411379d0d002469f57
PCNUOVO <PCNUOVO@ALESSIO1>
2025-06-22 12:47:38 +0200

    Aggiungere i file di progetto.

---
+++ Sito Territori/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.StaticFiles;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings[".kml"] = "application/vnd.google-earth.kml+xml";
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    ContentTypeProvider = provider
+});
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
